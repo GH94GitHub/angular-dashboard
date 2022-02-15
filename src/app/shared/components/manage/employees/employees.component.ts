70d1985bc5a985bd4b2b709c8f1f27026f83e054
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-02-15 16:28:02 -0600

    Added functionality: Add Employee, Delete Employee

--- src/app/shared/components/manage/employees/employees.component.ts
+++ src/app/shared/components/manage/employees/employees.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../../services/employee.service';
-import { Employee } from '../../../data/employee';
+import { Employee } from '../../../interfaces/employee.interface';
 import { DynamicComponent } from '../../../interfaces/dynamic-component.interface';
 import { DynamicComponentService } from '../../../services/dynamic-component.service';
 import { MessageService } from 'primeng/api';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../dialogs/confirm-dialog/confirm-dialog.component';
+import { AddEmployeeComponent } from '../../dialogs/add-employee/add-employee.component';
 
 @Component({
   selector: 'app-employees',
@@ -15,17 +18,79 @@ export class EmployeesComponent extends DynamicComponent implements OnInit {
   employees!: Employee[];
   styles = {
     width: 'fit-content',
-    height: '400px'
+    height: 'fit-content',
+    'max-height': '600px'
   };
 
   constructor(
     private empService: EmployeeService,
     dynamicComponentService: DynamicComponentService,
-    messageService: MessageService
+    messageService: MessageService,
+    private dialog: MatDialog
     ) { super(messageService, dynamicComponentService) }
 
   ngOnInit(): void {
     this.employees = this.empService.getAllEmployees();
   }
 
+  deleteEmployee(emp: Employee): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: 'Delete Employee',
+        content: `Are you sure you want to delete`,
+        items: `${emp.firstName} ${emp.lastName}: ${emp.title}`
+      }
+    });
+
+    dialogRef.afterClosed().subscribe( isConfirmed => {
+      if (isConfirmed) {
+
+        if (this.empService.deleteEmployee(emp.id)) {
+
+          const newArrayRef = Array(...this.employees);
+
+          const index = newArrayRef.findIndex( employee => {
+            if (emp.id === employee.id) return true
+            return;
+          });
+          newArrayRef.splice(index, 1);
+          this.employees = newArrayRef;
+
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Deleted',
+            detail: `Successfully deleted ${emp.firstName} ${emp.lastName}`
+          });
+        }
+      }
+    })
+  }
+
+  addEmployee(): void {
+    const dialogRef = this.dialog.open(AddEmployeeComponent);
+
+    dialogRef.afterClosed().subscribe( employee => {
+
+      const newEmp = this.empService.createEmployee(employee);
+
+      if (newEmp) {
+        const newArrayRef: Employee[] = Array(...this.employees);
+        newArrayRef.push(employee);
+
+        this.employees = newArrayRef;
+        this.messageService.add({
+          severity: 'success',
+          detail: 'Added',
+          summary: `Successfully added ${employee.firstName} ${employee.lastName}: ${employee.title}`
+        });
+
+      }
+      else
+        this.messageService.add({
+          severity: 'error',
+          detail: 'Failed',
+          summary: 'Failed to add employee'
+        });
+    })
+  }
 }
