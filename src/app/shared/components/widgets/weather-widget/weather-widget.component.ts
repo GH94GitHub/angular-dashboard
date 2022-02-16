4c42f3c00958f909c0482f54a0a0a43ea7e7d88a
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-02-16 11:45:09 -0600

    Revert "Code cleanup"
    This reverts commit 2c6d22870e7359f69e33f9752042fdc597d3d0e3.

--- src/app/shared/components/widgets/weather-widget/weather-widget.component.ts
+++ src/app/shared/components/widgets/weather-widget/weather-widget.component.ts
@@ -83,6 +83,7 @@ export class WeatherWidgetComponent extends DynamicComponent implements OnInit {
           direction: windDirection
         }
       }
+      console.log(this.weather);
       this.permissionGranted = true;
     })
 
