470ff12069c339ec04959932e05b968ef8eff135
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-02-28 10:50:42 -0600

    Testing `emitExit()` and `Exit Button`

--- src/app/shared/components/handle/handle.component.spec.ts
+++ src/app/shared/components/handle/handle.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { HandleComponent } from './handle.component';
 
@@ -21,5 +22,37 @@ describe('HandleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#emitExit', () => {
+    let exitSpy: {
+      next: jasmine.Spy
+    } = {} as any
+
+    beforeEach( () => {
+      exitSpy.next = spyOn(component.exit, 'next');
+    });
+
+    it('should emit "exit" to parent', () => {
+      component.emitExit();
+
+      expect(exitSpy.next).toHaveBeenCalledTimes(1);
+      expect(exitSpy.next).toHaveBeenCalledWith('exit')
+    });
+  });
+
+  describe('#btnExit', () => {
+    let btnExit: HTMLElement;
+    let emitExitSpy: jasmine.Spy;
+
+    beforeEach( () => {
+      btnExit = fixture.debugElement.query(By.css('.exit-button')).nativeElement;
+      emitExitSpy = spyOn(component, 'emitExit');
+    });
+
+    it('should call emitExit() when clicked', () => {
+      btnExit.click();
+      expect(emitExitSpy).toHaveBeenCalledTimes(1);
+    });
   });
 });
