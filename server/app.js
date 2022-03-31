43f665d6211b875efff41f99b808b23b5a0fb291
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-03-31 17:20:42 -0500

    Serves angular app

--- server/app.js
+++ server/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const weatherAPI = require('./routes/weather');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "..", "dist", "my-dashboard")));
+
 // APIs
 app.use("/api/weather", weatherAPI);
+
+app.get('*', (req, res) => {
+  const resp = fs.createReadStream(path.join(__dirname, "..", "dist", "my-dashboard", "index.html"));
+
+  res.writeHead(200, {"Content-Type": "text/html"});
+  resp.pipe(res);
+});
 
 module.exports = app;
